1e93dfd2b9f8c5be5dd9a64a9c265210f6f6d5ec
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-03-10 23:59:54 +0900

    private 주석

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@
 
 class Human {
   public name: string; // TS에서는 public private등 속성을 선언한다
-  private age: number;
+  public age: number;
   public gender: string;
+
+  // private로 선언하면  person.age 하는 식으로 외부에서 사용하는게 불가능 해진다. class 안에서만 쓸수 있게됨
   constructor(name: string, age: number, gender: string) {
     // 클래스가 시작할때마다 불리는것 gender? 라고 하면 선택으로 선언
     this.name = name;
@@ -22,9 +24,12 @@ const seongmin = new Human("seongmin", 18, "male");
 //   gender: "male",
 //   age: 24,
 // };
+
+// interface VS class -> class는 js 에서 구현이 되는데 ts는 구현되지 않음. ts적으로는 interface가 더 좋지만,
+// react나 node같은 다른 라이브러리가 있다면 interface 인식이 안되서 class를 쓰게됨
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(seongmin));
 export {};
