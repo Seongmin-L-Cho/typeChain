6dd7d91445f6890a0f24decafae335a40dea11e8
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-03-12 00:22:56 +0900

    function making by static

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    CryptoJS.SHA256(index + previousHash + timestamp + data);
+  };
+
   constructor(
     index: number,
     hash: string,
@@ -18,9 +30,16 @@ class Block {
   }
 }
 
+// Block.calculateBlockHash() // static이 아니면 바로 calculateBlockHash를 block에서 부를수는 없음
+
 const genesisBlock: Block = new Block(0, "121212", "", "hello", 1234568);
 
 let blockchain: [Block] = [genesisBlock];
 
-console.log();
+const getBlockChanin = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1]; // 블록체인 길이 알기용
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
 export {};
