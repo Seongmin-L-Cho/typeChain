5187eb92dc2132ee1bc50ba0766847d050fc597b
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-03-14 21:39:00 +0900

    create New Block

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ class Block {
     timestamp: number,
     data: string
   ): string => {
-    CryptoJS.SHA256(index + previousHash + timestamp + data);
+    return CryptoJS.SHA256(index + previousHash + timestamp + data);
   };
 
   constructor(
@@ -34,12 +34,32 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "121212", "", "hello", 1234568);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
 const getBlockChanin = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1]; // 블록체인 길이 알기용
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previosBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previosBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
 
 export {};
